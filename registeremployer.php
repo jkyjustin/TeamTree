372d7614e2beeb0399a3b4d5f4a042700fab4481
Dylan Yung <l9y8@ugrad.cs.ubc.ca>
2018-06-14 16:21:48 -0700

    register added

--- registeremployer.php
+++ registeremployer.php
@@ -8,18 +8,40 @@
 <body>  
 
 <?php
-// define variables and set to empty values
-$firstnameErr = $emailErr = $passwordErr = $companyErr = "";
-$firstname = $email = $password = $company = "";
+$firstnameErr = $lastname = $emailErr = $passwordErr = $companyErr = "";
+$firstname = $lastname = $email = $password = $company = $result = $companyId = "";
 
-$db_conn = OCILogon("ora_l9y8", "a47714118", "dbhost.ugrad.cs.ubc.ca:1522/ug");
+$success = True;
+$db_conn = OCILogon("ora_q7b7", "a68143064", "dbhost.ugrad.cs.ubc.ca:1522/ug");
+
+function executePlainSQL($cmdstr) {
+	global $db_conn, $success;
+	$statement = OCIParse($db_conn, $cmdstr);
+
+	if (!$statement) {
+		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+		$e = OCI_Error($db_conn);
+		echo htmlentities($e['message']);
+		$success = False;
+	}
+
+	$r = OCIExecute($statement, OCI_DEFAULT);
+	if (!$r) {
+		echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+		$e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+		echo htmlentities($e['message']);
+		$success = False;
+	} else {
+
+	}
+	return $statement;
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["firstname"])) {
     $firstnameErr = "First name is required";
   } else {
     $firstname = test_input($_POST["firstname"]);
-    // check if name only contains letters and whitespace
     if (!preg_match("/[a-zA-Z]/",$firstname)) {
       $firstnameErr = "Only letters allowed"; 
     }
@@ -29,7 +51,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $lastnameErr = "Last name is required";
   } else {
     $lastname = test_input($_POST["lastname"]);
-    // check if name only contains letters and whitespace
     if (!preg_match("/[a-zA-Z]/",$lastname)) {
       $lastnameErr = "Only letters allowed"; 
     }
@@ -39,16 +60,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $emailErr = "Email is required";
   } else {
     $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
       $emailErr = "Invalid email format"; 
     }
   }
     
-  if (empty($_POST["Company"])) {
-    $companyErr = "Company is required";
+  if (empty($_POST["company"])) {
+    $companyErr = "company is required";
   } else {
-    $company = test_input($_POST["Company"]);
+    if (get_query_count("SELECT * FROM Companies WHERE UPPER(name) = UPPER('{$_POST["company"]}')") < 1) {
+      $companyErr = "company does not exist";
+    } else {
+      $result = executePlainSQL("SELECT companyID FROM Companies WHERE UPPER(name) = UPPER('{$_POST["company"]}')");
+      $rslt2 = OCI_Fetch_Array($result, OCI_BOTH);
+      $companyId = $rslt2["companyID"];
+      $company = test_input($_POST["company"]);
+    }
   }
 
   if (empty($_POST["password"])) {
@@ -56,6 +83,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   } else {
     $password = test_input($_POST["password"]);
   }
+
+  if ($passwordErr == "" && $emailErr == "" && $companyErr == "" && $firstnameErr == "" && $lastnameErr !== "") {
+    $queryStr = "SELECT * FROM Accounts WHERE UPPER(fname) = UPPER('{$firstname}') AND UPPER(lname) = UPPER('{$lastname}') AND UPPER(email) = UPPER('{$email}')";
+
+    if(get_query_count($queryStr) > 0){
+      echo "This combination of first name, last name and email are taken";
+    } else {
+      $account_id = get_query_count("SELECT * FROM Accounts") + 1;
+      $employer_id = get_query_count("SELECT * FROM Employers") + 1;
+      executePlainSQL("INSERT INTO Accounts VALUES('{$account_id}', '{$firstname}', '{$lastname}', '{$email}', '{$password}', 0)");
+      executePLainSQL("INSERT INTO Employers VALUES ('{$employer_id}', '{$companyId}')");
+      OCI_commit($db_conn);
+      header("Location:./index.php");
+    }
+  }
+}
+
+function get_query_count($input) {
+  $result = executePlainSQL($input);
+  while($res = oci_fetch_array($result)){
+    $data[] = $res;
+  }
+  return count($data);
 }
 
 function test_input($data) {
@@ -66,7 +116,7 @@ function test_input($data) {
 }
 ?>
 
-<h2>Create Employer Account</h2>
+<h2>Create Student Account</h2>
 <p><span class="error">* required field</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
   First Name: <input type="text" name="firstname" value="<?php echo $firstname;?>">
@@ -81,7 +131,7 @@ function test_input($data) {
   Password: <input type="text" name="password" value="<?php echo $password;?>">
   <span class="error">* <?php echo $passwordErr;?></span>
   <br><br>
-  Company: <input type="text" name="Company" value="<?php echo $company;?>">
+  Company: <input type="text" name="company" value="<?php echo $company;?>">
   <span class="error">* <?php echo $companyErr;?></span>
   <br><br>
   <input type="submit" name="submit" value="Submit">  
@@ -98,6 +148,8 @@ echo "<br>";
 echo $password;
 echo "<br>";
 echo $company;
+echo "<br";
+echo $result;
 ?>
 
 </body>
--- registerstudent.php
+++ registerstudent.php
@@ -8,18 +8,40 @@
 <body>  
 
 <?php
-// define variables and set to empty values
-$firstnameErr = $emailErr = $passwordErr = $schoolErr = "";
-$firstname = $email = $password = $school = "";
+$firstnameErr = $lastname = $emailErr = $passwordErr = $schoolErr = "";
+$firstname = $lastname = $email = $password = $school = $result = $schoolId = "";
 
-$db_conn = OCILogon("ora_l9y8", "a47714118", "dbhost.ugrad.cs.ubc.ca:1522/ug");
+$success = True;
+$db_conn = OCILogon("ora_q7b7", "a68143064", "dbhost.ugrad.cs.ubc.ca:1522/ug");
+
+function executePlainSQL($cmdstr) {
+	global $db_conn, $success;
+	$statement = OCIParse($db_conn, $cmdstr);
+
+	if (!$statement) {
+		echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+		$e = OCI_Error($db_conn);
+		echo htmlentities($e['message']);
+		$success = False;
+	}
+
+	$r = OCIExecute($statement, OCI_DEFAULT);
+	if (!$r) {
+		echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+		$e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+		echo htmlentities($e['message']);
+		$success = False;
+	} else {
+
+	}
+	return $statement;
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["firstname"])) {
     $firstnameErr = "First name is required";
   } else {
     $firstname = test_input($_POST["firstname"]);
-    // check if name only contains letters and whitespace
     if (!preg_match("/[a-zA-Z]/",$firstname)) {
       $firstnameErr = "Only letters allowed"; 
     }
@@ -29,7 +51,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $lastnameErr = "Last name is required";
   } else {
     $lastname = test_input($_POST["lastname"]);
-    // check if name only contains letters and whitespace
     if (!preg_match("/[a-zA-Z]/",$lastname)) {
       $lastnameErr = "Only letters allowed"; 
     }
@@ -39,7 +60,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $emailErr = "Email is required";
   } else {
     $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
       $emailErr = "Invalid email format"; 
     }
@@ -48,7 +68,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["school"])) {
     $schoolErr = "School is required";
   } else {
-    $school = test_input($_POST["school"]);
+    if (get_query_count("SELECT * FROM Schools WHERE UPPER(sname) = UPPER('{$_POST["school"]}')") < 1) {
+      $schoolErr = "School does not exist";
+    } else {
+      $result = executePlainSQL("SELECT schoolID FROM Schools WHERE UPPER(sname) = UPPER('{$_POST["school"]}')");
+      $rslt2 = OCI_Fetch_Array($result, OCI_BOTH);
+      $schoolId = $rslt2["SCHOOLID"];
+      $school = test_input($_POST["school"]);
+    }
   }
 
   if (empty($_POST["password"])) {
@@ -56,6 +83,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   } else {
     $password = test_input($_POST["password"]);
   }
+
+  if ($passwordErr == "" && $emailErr == "" && $schoolErr == "" && $firstnameErr == "" && $lastnameErr !== "") {
+    $queryStr = "SELECT * FROM Accounts WHERE UPPER(fname) = UPPER('{$firstname}') AND UPPER(lname) = UPPER('{$lastname}') AND UPPER(email) = UPPER('{$email}')";
+
+    if(get_query_count($queryStr) > 0){
+      echo "This combination of first name, last name and email are taken";
+    } else {
+      $account_id = get_query_count("SELECT * FROM Accounts") + 1;
+      $student_id = get_query_count("SELECT * FROM Students") + 1;
+      executePlainSQL("INSERT INTO Accounts VALUES('{$account_id}', '{$firstname}', '{$lastname}', '{$email}', '{$password}', 0)");
+      executePLainSQL("INSERT INTO Students VALUES ('{$student_id}', '{$schoolId}')");
+      OCI_commit($db_conn);
+      header("Location:./index.php");
+    }
+  }
+}
+
+function get_query_count($input) {
+  $result = executePlainSQL($input);
+  while($res = oci_fetch_array($result)){
+    $data[] = $res;
+  }
+  return count($data);
 }
 
 function test_input($data) {
@@ -98,6 +148,8 @@ echo "<br>";
 echo $password;
 echo "<br>";
 echo $school;
+echo "<br";
+echo $result;
 ?>
 
 </body>
