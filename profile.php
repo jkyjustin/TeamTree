3620295e11c974c50e112c1dbb3ba52a094c62d1
Andrew Choi <h7h0b@anvil.ugrad.cs.ubc.ca>
2018-06-14 14:40:04 -0700

    finished reviews but can't get name of other student to show

--- profile.php
+++ profile.php
@@ -33,7 +33,6 @@ function executePlainSQL($cmdstr) {
 
 //$var = $_GET['acctID'];
 $var = 1;
-echo $var . "<br>";
 $result = NULL;
 
 $query = "SELECT * FROM Students NATURAL JOIN Accounts NATURAL JOIN Schools WHERE acctID ='{$var}'";
@@ -49,24 +48,52 @@ printResult($result);
 
 function printResult($result, $var){
 	while($row = OCI_Fetch_Array($result, OCI_BOTH)) {
-		echo $row["FNAME"] . " " . $row["LNAME"] . "<br><br>" . $row["SNAME"] . $row["NAME"]. "<br><br>";
+		echo $row["FNAME"] . " " . $row["LNAME"] . "<br>" . $row["SNAME"] . $row["NAME"]. "<br><br>";
 		
+		//Information for student profiles
 		if (!is_null($row["SNAME"])){
-			$given = executePlainSQL("SELECT DISTINCT * FROM Students NATURAL JOIN Accounts NATURAL JOIN Reviews NATURAL JOIN Schools WHERE reviewerID ='{$var}'");
 			echo "Your Reviews <br>";
-			echo "<table><tr><th>Given</th></tr>";
 			
-			while ($g = OCI_Fetch_Array($given, OCI_BOTH)){
+			echo "<table><tr><th>Received</th></tr>";
+			$receive = executePlainSQL("SELECT DISTINCT reviewID,reviewerID,courseNo,dept,sname,score,assignmentDesc,content,numLikes,numDislikes FROM Students NATURAL JOIN Accounts NATURAL JOIN Reviews NATURAL JOIN Schools WHERE revieweeID ='{$var}'");
+			
+			while ($r = OCI_Fetch_Array($receive, OCI_BOTH)){				
+				echo "<tr><td>" . $r["DEPT"]. " ". $r ["COURSENO"] . ", " . $r["SNAME"] . "<br>Given by ";
+				
+				//find reviewer name
+				$otherID = $r["REVIEWERID"];
+				echo $otherID;
+				$other = executePlainSQL("SELECT fname,lname FROM Accounts WHERE acctID ='{$otherID}'"); 
+				while ($o = OCI_Fetch_Array($other, OCI_BOTH));
+				echo $o["FNAME"] . " " . $o["LNAME"] . "<br>"; //prints nothing
+				
+				echo "Assignment: " . $r["ASSIGNMENTDESC"] . " Score: " . $r["SCORE"] . "<br>";
+				echo $r["CONTENT"] . "<br> Likes: " . $r["NUMLIKES"] . " Dislikes: " . $r["NUMDISLIKES"];
+				echo "</tr></td>";
+			}
+			echo "</table>";
+			
+			echo "<table><tr><th>Given</th></tr>";
+			$given = executePlainSQL("SELECT DISTINCT reviewID,revieweeID,courseNo,dept,sname,score,assignmentDesc,content,numLikes,numDislikes FROM Students NATURAL JOIN Accounts NATURAL JOIN Reviews NATURAL JOIN Schools WHERE reviewerID ='{$var}'");
+			
+			while ($g = OCI_Fetch_Array($given, OCI_BOTH)){				
+				echo "<tr><td>" . $g["DEPT"]. " ". $g ["COURSENO"] . ", " . $g["SNAME"] . "<br>Given by ";
+				
+				//find reviewee name
 				$otherID = $g["REVIEWEEID"];
 				echo $otherID;
-				$other = executePlainSQL("SELECT fname,lname FROM Accounts WHERE acctID ='{$otherID}'");
-				
-				echo "<tr><td>" . $g["DEPT"]. " ". $g ["COURSENO"] . ", " . $g["SNAME"] . "<br>given by";
+				$other = executePlainSQL("SELECT fname,lname FROM Accounts WHERE acctID ='{$otherID}'"); 
 				while ($o = OCI_Fetch_Array($other, OCI_BOTH));
-				echo $o["FNAME"] . " " . $o["LNAME"];
+				echo $o["FNAME"] . " " . $o["LNAME"] . "<br>"; //prints nothing
+				
+				echo "Assignment: " . $g["ASSIGNMENTDESC"] . " Score: " . $g["SCORE"] . "<br>";
+				echo $g["CONTENT"] . "<br> Likes: " . $g["NUMLIKES"] . " Dislikes: " . $g["NUMDISLIKES"];
+				echo "</tr></td>";
 			}
+			echo "</table>";
 			
 		}
+		//information for employer profiles
 		else if (!is_null($row["NAME"])){
 			echo endorse;
 		}
